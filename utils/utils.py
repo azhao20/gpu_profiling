f7bb3dae9036cb108e20038b4d289c7faae4adf5
ANDREW <azhaoy22@gmail.com>
2024-02-09 11:13:37 -0500

    Check before testing

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import os, sys
+import torch
 
 class HiddenPrints:
     """
@@ -11,4 +12,16 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
-    
+
+
+def get_precision(precision_flag: int):
+    precision_map = {
+        # 8: torch.float8_e5m2, # No support for float8 yet.
+        161: torch.bfloat16,
+        162: torch.float16,
+        32: torch.float32
+    }
+    if precision_flag not in precision_map:
+        print("Precision wasn't specified, defaulting to torch.float32")
+
+    return precision_map.get(precision_flag, torch.float32)
