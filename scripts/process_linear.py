5ba8aaa5da5cf8d2e7be8e37ad59c9007c86a575
ANDREW <azhaoy22@gmail.com>
2024-03-08 10:48:10 -0500

    Combined data, with some bugs

---
+++ scripts/process_linear.py
@@ -0,0 +1,16 @@
+import sys
+from utils import combine_data_csv
+
+def main():
+    ## Generate the combined csv.
+    ## TODO: add values.
+    combine_data_csv()
+
+    ## TODO: wrap this in another file; 
+    ## process the results of NCU.
+    ## E.g. extract metrics, etc, do some data science in a jupyter.
+
+    ## Then, join the two dataframes...
+
+if __name__ == "main":
+    main()
