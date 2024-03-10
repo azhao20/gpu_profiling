52df34940c69b7011042d726b1538b3a1daa2696
ANDREW <azhaoy22@gmail.com>
2024-03-10 18:16:31 -0400

    Rough draft for processing data, saving file issues

--- scripts/process_linear.py
+++ scripts/process_linear.py
@@ -1,10 +1,9 @@
 import sys
-from utils import combine_data_csv
+from utils import combine_profile_csv, get_unique_mapping, scale_csv
+# from utils import combine_data_csv
 
 def main():
     ## Generate the combined csv.
-    ## TODO: add values.
-    combine_data_csv()
 
     ## TODO: wrap this in another file; 
     ## process the results of NCU.
@@ -12,5 +11,14 @@ def main():
 
     ## Then, join the two dataframes...
 
-if __name__ == "main":
+    # combine_profile_csv()
+
+    # res = get_unique_mapping()
+    # for key, l in res.items():
+    #     if len(l) > 1:
+    #         print(f"{key}: {l}")
+
+    scale_csv()
+
+if __name__ == "__main__":
     main()
