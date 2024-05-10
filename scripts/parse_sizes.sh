f5812e63bcd8d14bd6986058a53acacddf5c66dd
Andrew Zhao <azhao@holygpu7c26203.rc.fas.harvard.edu>
2024-05-10 12:43:34 -0400

    [Parse] Updated parse scripts

--- scripts/parse_sizes.sh
+++ scripts/parse_sizes.sh
@@ -4,8 +4,8 @@
 export HOME="/n/holylabs/LABS/idreos_lab/Users/azhao"
 
 DATA_DIR=$HOME/gpu_profiling/data
-TRAIN_DIR=$DATA_DIR/new_train_models
-# EVAL_DIR=$DATA_DIR/eval_models
+TRAIN_DIR=$DATA_DIR/train_models
+EVAL_DIR=$DATA_DIR/eval_models
 FINAL_DIR=$DATA_DIR/final_models
 
 rm -rf $FINAL_DIR
@@ -13,16 +13,16 @@ mkdir -p $FINAL_DIR
 
 SCRIPT_DIR=$HOME/gpu_profiling/scripts
 
-# List of data source directories
+DATA_DIRECTORIES=($TRAIN_DIR $EVAL_DIR)
 DATA_SOURCES=("huggingface" "timm" "torchbench")
-
-# List of operation types
 OP_TYPES=("addmm" "bmm" "mm" "sdpea" "sdpfa" "conv")
 
 # Iterate over each data source and operation type
-for source in "${DATA_SOURCES[@]}"; do
-    for op in "${OP_TYPES[@]}"; do
-        echo "Processing op_type=$op from source=$source"
-        python3 "$SCRIPT_DIR/parse.py" --op_type "$op" --path "$TRAIN_DIR/$source" --save_path "$FINAL_DIR" --overwrite "a"
+for directory in "${DATA_DIRECTORIES[@]}"; do
+    for source in "${DATA_SOURCES[@]}"; do
+        for op in "${OP_TYPES[@]}"; do
+            echo "Processing op_type=$op from source=$source"
+            python3 "$SCRIPT_DIR/parse.py" --op_type "$op" --path "$directory/$source" --save_path "$FINAL_DIR" --overwrite "a"
+        done
     done
 done
