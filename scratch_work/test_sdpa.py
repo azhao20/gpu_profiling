b5f78271971e599e6fb8da6809fd4aa258a97bb3
Andrew Zhao <azhao@holygpu7c26202.rc.fas.harvard.edu>
2024-05-16 19:19:23 -0400

    [add]: math calls gpu

--- scratch_work/test_sdpa.py
+++ scratch_work/test_sdpa.py
@@ -2,9 +2,13 @@ import torch
 from torch.nn.functional import scaled_dot_product_attention
 from torch.nn.attention import SDPBackend, sdpa_kernel
 
-q32 = torch.randn(32, 32, 32, 32, dtype=torch.float32, device="cuda")
-q16 = torch.randn(32, 32, 32, 32, dtype=torch.float16, device="cuda")
-qb16 = torch.randn(32, 32, 32, 32, dtype=torch.bfloat16, device="cuda")
+from torch.profiler import profile, record_function
+
+device="cuda"
+
+q32 = torch.randn(32, 32, 32, 32, dtype=torch.float32, device=device)
+q16 = torch.randn(32, 32, 32, 32, dtype=torch.float16, device=device)
+qb16 = torch.randn(32, 32, 32, 32, dtype=torch.bfloat16, device=device)
 
 contexts = {
     "math": SDPBackend.MATH,
@@ -18,6 +22,9 @@ def fn(context, q, k, v):
 
 for context in contexts.values():
     print("context:", context)
-    fn(context, q16, q16, q16)
-    fn(context, qb16, qb16, qb16)
-    fn(context, q32, q32, q32)
+    with profile(activities=[torch.profiler.ProfilerActivity.CPU, 
+                         torch.profiler.ProfilerActivity.CUDA]) as prof:
+        fn(context, q16, q16, q16)
+        fn(context, qb16, qb16, qb16)
+        fn(context, q32, q32, q32)
+    print(prof.key_averages().table(sort_by="cuda_time_total"))
