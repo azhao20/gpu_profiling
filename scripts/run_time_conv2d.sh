8fe94700c879491c164d6b1671b664161c1ab3ad
ANDREW <azhaoy22@gmail.com>
2024-06-09 15:08:01 -0400

    [add]: conv, need testing

--- scripts/run_time_conv2d.sh
+++ scripts/run_time_conv2d.sh
@@ -5,16 +5,24 @@ OUTPUT_DIR="/n/holyscratch01/idreos_lab/Users/azhao/conv2d_times"
 
 module load python/3.10.12-fasrc01
 
-# TODO: take some of the values from time_conv2d.sh and move them here.
-sizes=()
+# Spin up 2 * 6^2 = 72 jobs.
+sizes=(2 8 32 128 512 1024) # 1024 for completeness.
+transposed=(0 1)
 
 # Uncomment for testing purposes
-# sizes=(16)
+sizes=(64)
+transposed=(0)
 
 mkdir -p $OUTPUT_DIR
 
-for n in "${sizes[@]}"
+for iH in "${sizes[@]}"
 do
-    JOB_FILE=$OUTPUT_DIR/$n
-    sbatch -o $JOB_FILE.%j.out -e $JOB_FILE.%j.err $SCRIPT_DIR/time_conv2d.sh $n
+    for iW in "${sizes[@]}"
+    do
+        for transposed in "${transposed[@]}"
+        do
+            JOB_FILE=$OUTPUT_DIR/$iH.$iW
+            sbatch -o $JOB_FILE.%j.out -e $JOB_FILE.%j.err $SCRIPT_DIR/time_conv2d.sh $iH $iW $transposed $1
+        done
+    done
 done
--- scripts/time_conv2d.sh
+++ scripts/time_conv2d.sh
@@ -1,76 +1,87 @@
 #!/bin/bash
 #SBATCH -c 8
+#SBATCH --mem=256000
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=apzhao@college.harvard.edu
 #SBATCH -t 7-00:00
 #SBATCH -p seas_gpu
-#SBATCH --mem=256000
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SSBATCH -t 0-12:00
 #SSBATCH -p gpu_test
 #SSBATCH --gres=gpu:1
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=apzhao@college.harvard.edu
 
 HOME="/n/holylabs/LABS/idreos_lab/Users/azhao"
 source $HOME/gpu_profiling/sh/initconda.sh
 
 SCRIPT_DIR=$HOME/gpu_profiling/scripts
-FINAL_DIR=$HOME/gpu_profiling/data/final/bmm
-FINAL_CSV=$FINAL_DIR/time.$1.csv
 
-# 3*5*11*11^2*8*6*4 * 2 = 7-8 million...
-dtypes=('16b' '16' '32')
-batch_sizes=(2 4 8 16 32)
-in_channel_sizes=(4 8 16 32 64 128 256 512 1024 2048 4096)
-sizes=(1 2 4 8 16 32 64 128 256 512 1024)
-out_channel_sizes=(16 32 64 128 256 512 1024 2048)
-group_sizes=(1 64 128 256 512 1024)
-kernel_sizes=(1 3 5 7)
-transposed=(0 1)
-# dilation_sizes=(1)
+if [ "$4" = "1" ]; then
+    FINAL_DIR=$HOME/gpu_profiling/data/final/conv2d_backward
+elif [ "$4" = "0" ]; then
+    FINAL_DIR=$HOME/gpu_profiling/data/final/conv2d
+else
+    echo "Invalid input for $4. Expected '0' or '1'."
+    exit 1
+fi
 
-dtypes=('32')
-batch_sizes=(16)
-in_channel_sizes=(2)
-sizes=(32)
-out_channel_sizes=(32)
-group_sizes=(32)
-kernel_sizes=(3)
-transposed=(0)
-# dilation_sizes=(1)
+FINAL_CSV=$FINAL_DIR/time.$1.$2.$3.csv
 
 # -p: ok if directory already exists.
 mkdir -p $FINAL_DIR
 
 # WARNING: this will delete the CSV if it already exists.
-rm $FINAL_CSV
+if [ -f "$FINAL_CSV" ]; then
+    echo "Deleting file $FINAL_CSV"
+    rm "$FINAL_CSV"
+fi
 
-for dtype in "${dtypes[@]}"
-do
-    for b in "${batch_sizes[@]}"
-    do
-        for in_channels in "${in_channel_sizes[@]}"
-        do
-            echo "$dtype, $b, $in_channels--------------" # For some sanity checking.
-            for iH in "${sizes[@]}"
-            do
-                for iW in "${sizes[@]}"
-                do
-                    for out_channels in "${out_channel_sizes[@]}"
-                    do
-                        for groups in "${group_sizes[@]}"
-                        do
-                            for kH in "${kernel_sizes[@]}"
-                            do
-                                for use_T in "${transposed[@]}"
-                                do
-                                    $HOME/env/bin/python3 \
-                                        $SCRIPT_DIR/conv2d.py --mode 'time' --dtype $dtype --b $b --in_channels $in_channels --iH $iH --iW $iW --out_channels $out_channels --groups 1 --kh $kH --kW $kW --stride 1 --dilation 1 --transposed $use_T --out_file $FINAL_CSV
-                                done
-                            done
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
+$HOME/env/bin/python3 $SCRIPT_DIR/conv2d.py --mode 'time' --iH $1 --iW $2 --transposed $3 --backward $4 --out_file $FINAL_CSV
+
+# dtypes=('16b' '16' '32')
+# batch_sizes=(2 4 8 16 32)
+# channel_sizes=(2 8 32 128 512 1024 2048 4096) # add 4096 for completeness.
+# sizes=(2 8 32 128 512 1024) # 1024 for completeness. ** 2.
+# group_sizes=(1 64 128 256 512 1024)
+# kernel_sizes=(3 5 7) ** 2
+# dilation_sizes=(1)
+
+# dtypes=('32')
+# batch_sizes=(16)
+# channel_sizes=(2)
+# sizes=(32)
+# group_sizes=(32)
+# kernel_sizes=(3)
+# transposed=(0)
+# dilation_sizes=(1)
+
+
+# for dtype in "${dtypes[@]}"
+# do
+#     for b in "${batch_sizes[@]}"
+#     do
+#         for in_channels in "${channel_sizes[@]}"
+#         do
+#             echo "$dtype, $b, $in_channels--------------" # For some sanity checking.
+#             for iH in "${sizes[@]}"
+#             do
+#                 for iW in "${sizes[@]}"
+#                 do
+#                     for out_channels in "${channel_sizes[@]}"
+#                     do
+#                         for groups in "${group_sizes[@]}"
+#                         do
+#                             for kH in "${kernel_sizes[@]}"
+#                             do
+#                                 for use_T in "${transposed[@]}"
+#                                 do
+#                                     $HOME/env/bin/python3 \
+#                                         $SCRIPT_DIR/conv2d.py --mode 'time' --dtype $dtype --b $b --in_channels $in_channels --iH $iH --iW $iW --out_channels $out_channels --groups 1 --kh $kH --kW $kW --stride 1 --dilation 1 --transposed $use_T --out_file $FINAL_CSV
+#                                 done
+#                             done
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
