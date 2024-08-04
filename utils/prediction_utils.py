72ac931e8b35ed099d66366f3952db63b6948bc7
ANDREW <azhaoy22@gmail.com>
2024-08-03 23:16:58 -0400

    [prediction]: sdpa and convolution

--- utils/prediction_utils.py
+++ utils/prediction_utils.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
 import statsmodels.api as sm
 
 from utils.time_utils import *
@@ -38,8 +38,8 @@ def get_train_test_split(X: pd.DataFrame, y: pd.DataFrame, return_concat: bool =
     """
     return_concat: useful for doing feature engineering
     """
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=random_seed)
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=random_seed)
 
     df_train = pd.concat([X_train, y_train], axis=1)
     df_val = pd.concat([X_val, y_val], axis=1)
@@ -50,8 +50,10 @@ def get_train_test_split(X: pd.DataFrame, y: pd.DataFrame, return_concat: bool =
 
 def get_r2_score(y, y_hat):
     mse = mean_squared_error(y, y_hat)
+    mape = mean_absolute_percentage_error(y, y_hat)
     r2 = r2_score(y, y_hat)
     print(f"Mean Squared Error: {mse}")
+    print(f"MAPE: {mape}")
     print(f"R-squared: {r2}")
     return mse, r2
     
