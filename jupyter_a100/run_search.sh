b9ad285519a64a086674bd492f3d60bfc8e546ed
ANDREW <azhaoy22@gmail.com>
2024-10-20 19:27:05 -0400

    [models]: updated gridsearch

--- jupyter_a100/run_search.sh
+++ jupyter_a100/run_search.sh
@@ -5,6 +5,8 @@
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=apzhao@college.harvard.edu
 #SBATCH -t 1-00:00
-#SBATCH -p seas_gpu
+#SBATCH -p seas_compute
+#SBATCH -o /n/holylabs/LABS/idreos_lab/Users/azhao/gpu_profiling/%A_%a.out
+#SBATCH -e /n/holylabs/LABS/idreos_lab/Users/azhao/gpu_profiling/%A_%a.err
 
 python jupyter_a100/main.py --op_type $1 --device $2 --n_iter $3
