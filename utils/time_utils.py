90c4130dffaf72fdef6e69ff84706aa86bb4cbd6
ANDREW <azhaoy22@gmail.com>
2024-10-20 20:21:05 -0400

    [scripts]: parquet scripts

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -51,8 +51,8 @@ class TimeProcessorMM(TimeProcessorBase):
     def get_data(self, sample_rate: float = 0.5):
         dfs = []
         for n in tqdm(self.n_values):
-            file_path = os.path.join(self.data_dir, f"time.{n}.pkl")
-            df = pd.read_pickle(file_path).sample(frac=sample_rate)
+            file_path = os.path.join(self.data_dir, f"time.{n}.parquet")
+            df = pd.read_parquet(file_path).sample(frac=sample_rate)
             df.rename(
                 columns={"Kernel Name": "kernel_params", "Latency (ms)": "time"},
                 inplace=True,
@@ -88,8 +88,8 @@ class TimeProcessorBMM(TimeProcessorBase):
     def get_data(self, sample_rate: float = 0.5):
         dfs = []
         for n in tqdm(self.n_values):
-            file_path = os.path.join(self.data_dir, f"time.{n}.pkl")
-            df = pd.read_pickle(file_path).sample(frac=sample_rate)
+            file_path = os.path.join(self.data_dir, f"time.{n}.parquet")
+            df = pd.read_parquet(file_path).sample(frac=sample_rate)
             df.rename(
                 columns={"Kernel Name": "kernel_params", "Latency (ms)": "time"},
                 inplace=True,
@@ -161,8 +161,8 @@ class TimeProcessorSDPA(TimeProcessorBase):
         backends = ["flash", "efficient", "cudnn"]
 
         for dtype, backend, h in tqdm(product(dtypes, backends, self.num_heads)):
-            file_path = os.path.join(self.data_dir, f"time.{dtype}.{backend}.{h}.pkl")
-            df = pd.read_pickle(file_path).sample(frac=sample_rate)
+            file_path = os.path.join(self.data_dir, f"time.{dtype}.{backend}.{h}.parquet")
+            df = pd.read_parquet(file_path).sample(frac=sample_rate)
             df.rename(
                 columns={
                     "Kernel Name": "kernel_params",
@@ -175,8 +175,8 @@ class TimeProcessorSDPA(TimeProcessorBase):
         dtype = "32"
         backend = "efficient"
         for h in tqdm(self.num_heads):
-            file_path = os.path.join(self.data_dir, f"time.{dtype}.{backend}.{h}.pkl")
-            df = pd.read_pickle(file_path).sample(frac=sample_rate)
+            file_path = os.path.join(self.data_dir, f"time.{dtype}.{backend}.{h}.parquet")
+            df = pd.read_parquet(file_path).sample(frac=sample_rate)
             df.rename(
                 columns={"Kernel Name": "kernel_params", "Latency (ms)": "time"},
                 inplace=True,
@@ -379,12 +379,12 @@ class TimeProcessorConv2d(TimeProcessorBase):
         for iH, transposed, group_size, batch_size in tqdm(
             product(self.iH, self.transposed, self.group_sizes, self.batch_sizes)
         ):
-            file_name = f"time.{iH}.{transposed}.{group_size}.{batch_size}.pkl"
+            file_name = f"time.{iH}.{transposed}.{group_size}.{batch_size}.parquet"
             file_path = os.path.join(self.data_dir, file_name)
             if not os.path.isfile(file_path):
                 missing_file_count += 1
                 continue
-            df = pd.read_pickle(file_path).sample(frac=sample_rate)
+            df = pd.read_parquet(file_path).sample(frac=sample_rate)
             df.rename(
                 columns={
                     "Kernel Name": "kernel_params",
