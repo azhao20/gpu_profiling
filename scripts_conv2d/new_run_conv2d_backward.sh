557f04cd2aacf526a817dc810fd37691d56df959
Andrew Zhao <azhao@holygpu7c26105.rc.fas.harvard.edu>
2024-10-22 18:46:51 -0400

    [fix]: conv2d backward save dir typo

--- scripts_conv2d/new_run_conv2d_backward.sh
+++ scripts_conv2d/new_run_conv2d_backward.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="/n/holylabs/LABS/idreos_lab/Users/azhao/gpu_profiling/scripts_conv2d
 OUTPUT_DIR="/n/holyscratch01/idreos_lab/Users/azhao/conv2d_times"
 
 # Save to scratch now.
-FINAL_DIR="/n/holyscratch01/idreos_lab/Users/azhao/conv2d_data"
+FINAL_DIR="/n/holyscratch01/idreos_lab/Users/azhao/conv2d_backward_data"
 
 # -p: ok if directory already exists.
 mkdir -p $FINAL_DIR
